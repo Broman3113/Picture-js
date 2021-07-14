5fec17d43691bff19d7ede033984126e1f131ec2
Andrey Borbot <broman3113@gmail.com>
2021-07-14 12:01:50 +0300

    Added the ability to run without json-server

--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -3,22 +3,16 @@ import {getResource} from "../services/requests";
 const showMoreStyles = (trigger, wrapper) => {
     const btn = document.querySelector(trigger);
 
-    // cards.forEach(card => {
-    //     card.classList.add('animated', 'fadeInUp');
-    // });
+    // btn.addEventListener("click", function () { // For db json-server src/assets/db.json
+    //     getResource('http://localhost:3000/styles')
+    //         .then(res => createCards(res))
+    //         .catch(error => console.log(error));
     //
-    // btn.addEventListener('click', () => {
-    //     cards.forEach(card => {
-    //         card.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
-    //         card.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
-    //     });
-    //     btn.style.display = "none";
-    //     btn.remove();
+    //     this.remove();
     // });
-
-    btn.addEventListener("click", function () {
-        getResource('http://localhost:3000/styles')
-            .then(res => createCards(res))
+    btn.addEventListener("click", function () { // Without json-server
+        getResource('assets/db.json')
+            .then(res => createCards(res.styles))
             .catch(error => console.log(error));
 
         this.remove();
